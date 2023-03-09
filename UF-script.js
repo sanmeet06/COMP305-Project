4724f608e1f1579f6ae64456b06efc68c461bd6f
syedShahrukh96 <syed.shahrukh33@gmail.com>
2023-03-09 02:44:47 -0600

    user Fav by shahrukh

---
+++ UF-script.js
@@ -0,0 +1,45 @@
+ 
+const cardButtons = document.querySelectorAll('.User_button');
+
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    disableOnInteraction: true, 
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+// var swiper = new Swiper('.slide-content', {
+//     slidesPerView: 3,
+//     spaceBetween: 20,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     noSwiping: true,
+//     noSwipingClass: 'User_button',
+//   });
